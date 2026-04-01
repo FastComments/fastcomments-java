b53fa8e2aca613d8fb7ca9c6bb93af23d5f06112
winrid <winrid@gmail.com>
2026-04-01 15:31:18 -0700

    automate java 25 upgrade

--- update.sh
+++ update.sh
@@ -9,5 +9,8 @@ rm -rvf ./client/src ./client/docs \
         --additional-properties=useSingleRequestParameter=true,disallowAdditionalPropertiesIfNotPresent=false \
         -c config.json
 
-# Change to the desired Gradle version
+# Patch generated build files for Gradle 9 / Java 25 compatibility
 sed -i 's|distributionUrl=.*|distributionUrl=https\\://services.gradle.org/distributions/gradle-9.4.1-bin.zip|' ./client/gradle/wrapper/gradle-wrapper.properties
+sed -i 's|spotless-plugin-gradle:6.11.0|spotless-plugin-gradle:8.4.0|' ./client/build.gradle
+sed -i '/sourceCompatibility = JavaVersion.VERSION_1_8/{N;s|sourceCompatibility = JavaVersion.VERSION_1_8\n    targetCompatibility = JavaVersion.VERSION_1_8|java {\n        sourceCompatibility = JavaVersion.VERSION_1_8\n        targetCompatibility = JavaVersion.VERSION_1_8\n    }|}' ./client/build.gradle
+sed -i 's|main = System.getProperty|mainClass = System.getProperty|' ./client/build.gradle
